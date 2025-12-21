afa3b49370478154747c1617608f8b0f61b89cc4
Jayson Zhong <jayson_zhong@apple.com>
2025-12-21 12:37:05 +0800

    Scaffold React frontend application

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TAL Hackathon Demo</h1>
+        <p>Welcome to the demo application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
